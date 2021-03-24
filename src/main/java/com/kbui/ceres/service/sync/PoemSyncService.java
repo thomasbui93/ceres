821b0946d7d61898486555b2333cf4c394515943
Khoa Bui <kbui@zendesk.com>
2021-03-24 03:28:07 +0000

    Avoid failure that corrupt the task

--- src/main/java/com/kbui/ceres/service/sync/PoemSyncService.java
+++ src/main/java/com/kbui/ceres/service/sync/PoemSyncService.java
@@ -4,19 +4,15 @@ import com.kbui.ceres.config.CrawlerPoemConfiguration;
 import com.kbui.ceres.repository.PoemRepository;
 import com.kbui.ceres.repository.PoetRepository;
 import com.kbui.ceres.repository.models.Poet;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.ReplaceOptions;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import java.util.Arrays;
-import org.bson.BsonDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.UpdateDefinition;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
@@ -47,8 +43,19 @@ public class PoemSyncService {
   private Future<Boolean> syncDataPerAuthor(String author) {
     return poetRepository.get(author)
         .map(poet -> {
-          var poetDocument = mongoTemplate.insert(poet.toPoetDocument(), "poets");
-          return Pair.of(poet, poetDocument);
+          try {
+            var poetDocument = mongoTemplate.insert(poet.toPoetDocument(), "poets");
+            return Pair.of(poet, poetDocument);
+          } catch (Exception ex) {
+
+            var query = new Query(Criteria.where("url").is(poet.getPoetUrl()));
+            var poetDoc = mongoTemplate.findOne(query, PoetDocument.class, "poets");
+            if (poetDoc == null) {
+              log.error("Failed to sync poet to db", ex);
+              return Pair.of(poet, PoetDocument.builder().build());
+            }
+            return Pair.of(poet, poetDoc);
+          }
         })
         .compose(r -> syncAuthorPoems(r.getFirst(), r.getSecond()));
   }
@@ -57,6 +64,7 @@ public class PoemSyncService {
     var links = poet.getLinks();
     var futures = links.stream().map(link -> mapPoemToMongoDb(link, poetDocument))
         .toArray(Future[]::new);
+    if (poetDocument.getId() == null) return Future.failedFuture("Empty poet.");
     return CompositeFuture.all(Arrays.asList(futures))
         .compose(s -> {
           if (s.failed()) {
@@ -69,18 +77,21 @@ public class PoemSyncService {
   }
 
   private Future<PoemDocument> mapPoemToMongoDb(String link, PoetDocument poetDocument) {
-    return poemRepository.get(link)
-        .compose(poemEntity -> {
-          try {
-            if (poemEntity.getContent().size() == 0) {
-              return Future.succeededFuture();
-            }
-            var poemDocument = poemEntity.toPoemDocument(poetDocument);
-            var poem = mongoTemplate.insert(poemDocument, "poems");
-            return Future.succeededFuture(poem);
-          } catch (Exception ex) {
-            return Future.failedFuture(ex);
-          }
-        });
+    return poemRepository
+        .get(link)
+        .compose(
+            poemEntity -> {
+              try {
+                if (poemEntity.getContent().size() == 0) {
+                  return Future.succeededFuture();
+                }
+                var poemDocument = poemEntity.toPoemDocument(poetDocument);
+                var poem = mongoTemplate.insert(poemDocument, "poems");
+                return Future.succeededFuture(poem);
+              } catch (Exception ex) {
+                log.error("Failed to sync poem to db", ex);
+                return Future.succeededFuture(PoemDocument.builder().build());
+              }
+            });
   }
 }
